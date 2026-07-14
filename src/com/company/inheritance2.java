e5e9431f32c09d23715888ce19615c9efb241d2d
owais8904 <owaisahmed8904@gmail.com>
2026-07-14 20:08:39 +0530

    inheritance practice

--- src/com/company/inheritance2.java
+++ src/com/company/inheritance2.java
@@ -1,4 +1,36 @@
 package com.company;
+class person{
+    String name;
+    int age;
+   public  void setDetails(String n,int a){
+       this.name=n;
+       this.age=a;
+    }
+    public void displayDetails(){
+        System.out.println("Name- "+this.name);
+        System.out.println("Age- "+this.age);
+    }
+}
+class Student1 extends person {
+    int marks;
 
+    public void setMarks(int marks) {
+        this.marks = marks;
+
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+}
 public class inheritance2 {
+    static void main(String[] args) {
+        Student1 s=new Student1();
+        s.setDetails("owais",20);
+        s.displayDetails();
+        s.setMarks(90);
+        System.out.println("Marks- "+s.getMarks());
+
+    }
 }
